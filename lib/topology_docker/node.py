6382bbbb4bfd95c645ad27ded28b6698940073a3
Pablo Saenz <psaenz@hpe.com>
2018-03-09 13:22:59 -0800

    sss:wq

--- lib/topology_docker/node.py
+++ lib/topology_docker/node.py
@@ -132,7 +132,7 @@ class DockerNode(CommonNode):
                 # Avoid connecting to host bridge, usually docker0
                 network_mode=network_mode,
                 mem_limit='3840m', cpu_period=100000,
-                cpu_quota=180000, cap_add=["sys_nice"],
+                cpu_quota=180000, 
                 binds=container_binds
             )
         else:
