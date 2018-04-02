acc1297a2ac4c8916cf36656246bdd1419faf95e
Pablo Saenz <psaenz@hpe.com>
2018-04-02 15:07:31 -0700

    Revert "suqash later"
    This reverts commit d4a9e07ef84fc959741fdfc49827193a4f2ad5b0.

--- lib/topology_docker/node.py
+++ lib/topology_docker/node.py
@@ -128,7 +128,8 @@ class DockerNode(CommonNode):
             # Create host config
             self._host_config = self._client.create_host_config(
                 # Container is given access to all devices
-                cap_add=['ALL'],
+                cap_add=['ALL'], cpu_quota=180000,
+                mem_limit='7680m', cpu_period=100000,
                 # Avoid connecting to host bridge, usually docker0
                 network_mode=network_mode,
                 binds=container_binds
@@ -140,7 +141,7 @@ class DockerNode(CommonNode):
                 privileged=True,
                 # Avoid connecting to host bridge, usually docker0
                 network_mode=network_mode,
-                mem_limit='3840m', cpu_period=100000,
+                mem_limit='7680m', cpu_period=100000,
                 cpu_quota=180000, cap_add=["sys_nice"],
                 binds=container_binds
             )
